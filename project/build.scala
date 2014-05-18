3714362b5feba7203f783ebf7fd7f89c158eccda
Alexander Vykhodtsev <avflance@gmail.com>
2014-05-19 02:34:35 +0700

    Use short project name for SBT and prefixed names for publishing

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,8 @@ object InfluxBuild extends Build {
   val org = "org.influxdb"
 
   // the core API is compatible with scala 2.10+ and only depends on slf4j
-  lazy val core = Project("influxdb-scala-core", file("core")) settings(
+  lazy val core = Project("core", file("core")) settings(
+      name := "influxdb-scala-core",
       organization := org,
       version := "0.6-SNAPSHOT",
       scalaVersion := "2.10.3",
@@ -21,7 +22,8 @@ object InfluxBuild extends Build {
   // that use async-http-client and json4s. In a Play! framework application, you may want to provide
   // different implementations based on WS and the json macros available in play to avoid these redundant
   // dependencies
-  lazy val standalone = Project("influxdb-scala-standalone", file("standalone")) dependsOn(core) settings(
+  lazy val standalone = Project("standalone", file("standalone")) dependsOn(core) settings(
+      name := "influxdb-scala-standalone",
       organization := org,
       version := "0.6-SNAPSHOT",
       scalaVersion := "2.10.3",
@@ -42,7 +44,8 @@ object InfluxBuild extends Build {
   /* I give up... Why doesn't this work?????????
 
   // this project contains the Map[String,Any] <-> case class mapping macro and only builds with scala 2.11
-  lazy val macro = Project("influxdb-scala-macro", file("macro")) settings(
+  lazy val macro = Project("macro", file("macro")) settings(
+      name := "influxdb-scala-macro",
       organization := org,
       version := "0.1",
       scalaVersion := "2.11.0-M7",
@@ -53,7 +56,8 @@ object InfluxBuild extends Build {
 
   // this project contains the typed API which depends on both the core untyped API as well as the macros
   // which unfortunately only compile with the 2.11 milestone of scala
-  lazy val experimental = Project("influxdb-scala-experimental", file("experimental")) dependsOn(core, macro, standalone) settings(
+  lazy val experimental = Project("experimental", file("experimental")) dependsOn(core, macro, standalone) settings(
+      name := "influxdb-scala-experimental",
       organization := org,
       version := "0.1",
       scalaVersion := "2.11.0-M7"
